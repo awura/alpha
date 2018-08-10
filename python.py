790e617a6d5376c38b1e3398136a847fe9bd0e70
awura <40544710+awura@users.noreply.github.com>
2018-08-10 09:55:06 +0100

    very incomplete. would work on it again soon.

---
+++ python.py
@@ -0,0 +1,65 @@
+import random 
+
+
+randnum = {}
+randnum = (random.uniform(-5,7))
+
+def randset():
+  totalsum = 0
+    for x in range (50):
+      randlist = list(randnum)
+      totalsum += randlist
+  y = sort(randlist)
+
+def randprint():
+  print(randset.y.append)
+
+def randMin():
+  print(min(randnum))
+
+def randMax():
+  print(max(randnum))
+
+def randMean():
+  randavg = randset.totalsum/50
+  print(randavg)
+
+def randall():
+  print("Numbers (sorted) : ", randprint)
+  print("Maximum: ", randMax)
+  print("Minimum: ", randMin)
+  print("Mean: ", randMean) 
+
+###
+import random 
+
+Class randcalc:
+  randnum = {}
+  randnum = (random.uniform(-5,7))
+
+  def randset():
+  totalsum = 0
+    for x in range (50):
+      randlist = list(randnum)
+      totalsum += randlist
+    y = sort(randlist)
+
+  def randprint():
+    z = randset.y
+    print(y.append)
+
+  def randMin():
+    print(min(randnum))
+
+  def randMax():
+    print(max(randnum))
+
+  def randMean():
+    randavg = randset.totalsum/50
+    print(randavg)
+
+  def randall():
+    print("Numbers (sorted) : ", randprint())
+    print("Maximum: ", randMax())
+    print("Minimum: ", randMin())
+    print("Mean: ", randMean())
